2e6a2b6aecc0b0e41399b5b3bf5b09f146ca3335
Yonmoji-X <yametu55@gmail.com>
2024-06-02 14:38:34 +0900

    大体の構成作成

---
+++ js/firebase.js
@@ -0,0 +1,19 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+  authDomain: "skill-records-f67d6.firebaseapp.com",
+  projectId: "skill-records-f67d6",
+  storageBucket: "skill-records-f67d6.appspot.com",
+  messagingSenderId: "684855358016",
+  appId: "1:684855358016:web:7d3bcde894a6a2eacc25a5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
